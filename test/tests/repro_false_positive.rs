84902fbc1647f163a4e52e581e1e2c39b4441b8d
emirongrr <emirongrr@gmail.com>
2026-04-08 00:44:48 +0300

    test: add false positive reproduction for async tests

---
+++ test/tests/repro_false_positive.rs
@@ -0,0 +1,11 @@
+#[tokio::test]
+async fn test_false_positive_repro() {
+    tokio::spawn(async {
+        panic!("This should fail the test but might not!");
+    });
+    
+    // Give some time for the panic to happen
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    
+    println!("Test finishing - if it passes, we have a false positive!");
+}
